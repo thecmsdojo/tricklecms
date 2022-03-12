d5191012d81570efc68b9f5b4d4733f3f7be0d1f
Jim Huang <thecmsdojo@gmail.com>
2022-03-12 15:33:07 -0800

    fix environment variable for start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,10 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+if [ $# -eq 0 ]; then
+  echo "missing environment"
+  echo "sample usage: sh start_server.sh local"
+  exit
+fi
+
 cd $SCRIPT_DIR/../build/$1
 docker-compose up --build
